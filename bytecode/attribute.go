cf8b3ddba41e011748c3141d83b50ffd21199a0a
lanhuidong <472897576@qq.com>
2022-12-09 16:10:49 +0800

    feat: 解析Code

--- bytecode/attribute.go
+++ bytecode/attribute.go
@@ -26,25 +26,28 @@ func parse(data []byte, index int, constantPool []ConstantPoolInfo) (int, Attrib
 	switch base.Name {
 	case "ConstantValue":
 		item = &ConstantValue{}
-		item.parse(base, info)
+		item.parse(base, info, constantPool)
+	case "Code":
+		item = &Code{}
+		item.parse(base, info, constantPool)
 	case "InnerClasses":
 		item = &InnerClasses{}
-		item.parse(base, info)
+		item.parse(base, info, constantPool)
 	case "EnclosingMethod":
 		item = &EnclosingMethod{}
-		item.parse(base, info)
+		item.parse(base, info, constantPool)
 	case "SourceFile":
 		item = &SourceFile{}
-		item.parse(base, info)
+		item.parse(base, info, constantPool)
 	case "SourceDebugExtension":
 		item = &SourceDebugExtension{}
-		item.parse(base, info)
+		item.parse(base, info, constantPool)
 	case "BootstrapMethods":
 		item = &BootstrapMethods{}
-		item.parse(base, info)
+		item.parse(base, info, constantPool)
 	case "NestMembers":
 		item = &NestMembers{}
-		item.parse(base, info)
+		item.parse(base, info, constantPool)
 	default:
 		fmt.Printf("attribue name is %s\n", base.Name)
 	}
@@ -52,7 +55,7 @@ func parse(data []byte, index int, constantPool []ConstantPoolInfo) (int, Attrib
 }
 
 type AttributeInfo interface {
-	parse(base *AttributeBase, data []byte)
+	parse(base *AttributeBase, data []byte, constantPool []ConstantPoolInfo)
 	GetName() string
 	String(constantPool []ConstantPoolInfo) string
 }
@@ -72,13 +75,65 @@ type ConstantValue struct {
 	ConstantValueIndex uint16
 }
 
-func (c *ConstantValue) parse(base *AttributeBase, data []byte) {
+func (c *ConstantValue) parse(base *AttributeBase, data []byte, constantPool []ConstantPoolInfo) {
 	c.AttributeBase = *base
 	binary.Read(bytes.NewBuffer(data), binary.BigEndian, &c.ConstantValueIndex)
 }
 
 func (c *ConstantValue) String(constantPool []ConstantPoolInfo) string {
 	return constantPool[c.ConstantValueIndex].String(constantPool)
+}
+
+type ExceptionTable struct {
+	StartPc   uint16
+	EndPc     uint16
+	HandlerPc uint16
+	CatchType uint16
+}
+
+func (e *ExceptionTable) Parse(data []byte, index int) {
+	binary.Read(bytes.NewBuffer(data[0:2]), binary.BigEndian, &e.StartPc)
+	binary.Read(bytes.NewBuffer(data[2:4]), binary.BigEndian, &e.EndPc)
+	binary.Read(bytes.NewBuffer(data[4:6]), binary.BigEndian, &e.HandlerPc)
+	binary.Read(bytes.NewBuffer(data[6:8]), binary.BigEndian, &e.CatchType)
+}
+
+type Code struct {
+	AttributeBase
+	MaxStack             uint16
+	MaxLocals            uint16
+	CodeLength           uint32
+	Code                 []byte
+	ExceptionTableLength uint16
+	Table                []ExceptionTable
+	AttributesCount      uint16
+	Attributes           []AttributeInfo
+}
+
+func (c *Code) parse(base *AttributeBase, data []byte, constantPool []ConstantPoolInfo) {
+	c.AttributeBase = *base
+	binary.Read(bytes.NewBuffer(data[0:2]), binary.BigEndian, &c.MaxStack)
+	binary.Read(bytes.NewBuffer(data[2:4]), binary.BigEndian, &c.MaxLocals)
+	binary.Read(bytes.NewBuffer(data[4:8]), binary.BigEndian, &c.CodeLength)
+	c.Code = data[8 : 8+c.CodeLength]
+	index := 8 + int(c.CodeLength)
+	binary.Read(bytes.NewBuffer(data[index:index+2]), binary.BigEndian, &c.ExceptionTableLength)
+	index += 2
+	for i := 0; i < int(c.ExceptionTableLength); i++ {
+		table := &ExceptionTable{}
+		table.Parse(data, index)
+		index += 8
+		c.Table = append(c.Table, *table)
+	}
+	binary.Read(bytes.NewBuffer(data[index:index+2]), binary.BigEndian, &c.AttributesCount)
+	index += 2
+	_, attrs := ParseAttribute(int(c.AttributesCount), data, index, constantPool)
+	c.Attributes = attrs
+}
+
+func (c *Code) String(constantPool []ConstantPoolInfo) string {
+	result := fmt.Sprintf("max stack: %d, max locals: %d\n", c.MaxStack, c.MaxLocals)
+	return result
 }
 
 type InnerClasses struct {
@@ -102,7 +157,7 @@ func (i *InnerClassInfo) parse(data []byte, index int) int {
 	return index + 8
 }
 
-func (i *InnerClasses) parse(base *AttributeBase, data []byte) {
+func (i *InnerClasses) parse(base *AttributeBase, data []byte, constantPool []ConstantPoolInfo) {
 	i.AttributeBase = *base
 	binary.Read(bytes.NewBuffer(data[0:2]), binary.BigEndian, &i.NumberOfClasses)
 	i.Classes = make([]InnerClassInfo, i.NumberOfClasses)
@@ -128,7 +183,7 @@ type EnclosingMethod struct {
 	MethodIndex uint16
 }
 
-func (e *EnclosingMethod) parse(base *AttributeBase, data []byte) {
+func (e *EnclosingMethod) parse(base *AttributeBase, data []byte, constantPool []ConstantPoolInfo) {
 	e.AttributeBase = *base
 	binary.Read(bytes.NewBuffer(data[0:2]), binary.BigEndian, &e.ClassIndex)
 	binary.Read(bytes.NewBuffer(data[2:4]), binary.BigEndian, &e.MethodIndex)
@@ -143,7 +198,7 @@ type SourceFile struct {
 	SourceFileIndex uint16
 }
 
-func (s *SourceFile) parse(base *AttributeBase, data []byte) {
+func (s *SourceFile) parse(base *AttributeBase, data []byte, constantPool []ConstantPoolInfo) {
 	s.AttributeBase = *base
 	binary.Read(bytes.NewBuffer(data), binary.BigEndian, &s.SourceFileIndex)
 }
@@ -158,7 +213,7 @@ type NestMembers struct {
 	Classes         []uint16
 }
 
-func (n *NestMembers) parse(base *AttributeBase, data []byte) {
+func (n *NestMembers) parse(base *AttributeBase, data []byte, constantPool []ConstantPoolInfo) {
 	n.AttributeBase = *base
 	binary.Read(bytes.NewBuffer(data[0:2]), binary.BigEndian, &n.NumberOfClasses)
 	n.Classes = make([]uint16, n.NumberOfClasses)
@@ -184,7 +239,7 @@ type SourceDebugExtension struct {
 	DebugExtension []uint8
 }
 
-func (s *SourceDebugExtension) parse(base *AttributeBase, data []byte) {
+func (s *SourceDebugExtension) parse(base *AttributeBase, data []byte, constantPool []ConstantPoolInfo) {
 	s.AttributeBase = *base
 	binary.Read(bytes.NewBuffer(data), binary.BigEndian, &s.DebugExtension)
 }
@@ -219,7 +274,7 @@ func (b *BootStrapMethod) parse(data []byte, index int) int {
 	return index
 }
 
-func (b *BootstrapMethods) parse(base *AttributeBase, data []byte) {
+func (b *BootstrapMethods) parse(base *AttributeBase, data []byte, constantPool []ConstantPoolInfo) {
 	b.AttributeBase = *base
 	binary.Read(bytes.NewBuffer(data[0:2]), binary.BigEndian, &b.Num)
 	b.Methods = make([]BootStrapMethod, b.Num)
